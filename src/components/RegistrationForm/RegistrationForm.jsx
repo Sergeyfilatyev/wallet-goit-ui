6cb76cc102d95246c69524868dfd7108f0073c42
nickgric <nick.gric@gmail.com>
2023-04-06 17:47:51 -0400

    chakra form

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,5 +1,14 @@
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text,
+} from "@chakra-ui/react";
+
+import { EmailIcon, LockIcon, UnlockIcon, StarIcon } from "@chakra-ui/icons";
 
 import { validationSchema } from "../../utils/validationSchema";
 
@@ -28,30 +37,62 @@ export const RegistrationForm = () => {
         }}
       >
         <Form>
-          <Field name="email" placeholder="E-mail" autoComplete="new-email" />
-          <ErrorMessage name="email" />
-          <Field
-            type="password"
-            name="password"
-            placeholder="Password"
-            autoComplete="new-password"
-          />
-          <ErrorMessage name="password" />
-          <Field
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm password"
-            autoComplete="new-password"
-          />
-          <ErrorMessage name="confirmPassword" />
-          <Field
-            name="firstName"
-            placeholder="First name"
-            autoComplete="new-name"
-          />
-          <ErrorMessage name="firstName" />
-          <button type="submit">Register</button>
-          <a href="/">Log In</a>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<EmailIcon color="gray.300" />}
+            />
+            <Input
+              as={Field}
+              name="email"
+              placeholder="E-mail"
+              autoComplete="new-email"
+            />
+          </InputGroup>
+          <Text as={ErrorMessage} name="email" fontSize="6xl" />
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<UnlockIcon color="gray.300" />}
+            />
+            <Input
+              as={Field}
+              type="password"
+              name="password"
+              placeholder="Password"
+              autoComplete="new-password"
+            />
+          </InputGroup>
+          <Text as={ErrorMessage} name="password" />
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<LockIcon color="gray.300" />}
+            />
+            <Input
+              as={Field}
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm password"
+              autoComplete="new-password"
+            />
+          </InputGroup>
+          <Text as={ErrorMessage} name="confirmPassword" />
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<StarIcon color="gray.300" />}
+            />
+            <Input
+              as={Field}
+              name="firstName"
+              placeholder="First name"
+              autoComplete="new-name"
+            />
+          </InputGroup>
+          <Text as={ErrorMessage} name="firstName" />
+          <Button type="submit">Register</Button>
+          <Button as={"a"}>Log In</Button>
         </Form>
       </Formik>
     </Box>
