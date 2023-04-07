79a06e02c294c36f79af9f4d4bb62254a5f3ad8e
nickgric <nick.gric@gmail.com>
2023-04-07 19:30:43 -0400

    1

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router-dom";
 
 import { Header } from "../components/Header";
 
+import { Currency } from "../components/Currency";
+
 const DashboardPage = () => {
   return (
     <>
       <Header />
       <main>
         <Outlet />
+        <Currency />
       </main>
     </>
   );
