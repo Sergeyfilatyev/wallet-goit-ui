caac2bfd81514482feb5d183e8f44b09f5803b86
nickgric <nick.gric@gmail.com>
2023-04-08 02:13:29 -0400

    login-and-register

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,10 @@
-import { Box, Image, Heading, Flex } from "@chakra-ui/react";
+import { Box, Image, Flex } from "@chakra-ui/react";
 import Media from "react-media";
 
 import { LoginForm } from "../components/LoginForm";
 import { SecondaryLogo } from "../components/SecondaryLogo";
 
+import loginPicture from "../assets/images/login-picture.svg";
 import purpleEllips from "../assets/images/purple-ellips-background.png";
 import orangeEllips from "../assets/images/orange-ellips-background.png";
 
@@ -21,34 +22,48 @@ const LoginPage = () => {
             backgroundImage={purpleEllips}
             backgroundRepeat="no-repeat"
             backgroundPosition="bottom left"
-            backgroundSize="50%"
+            backgroundSize={{ m: "75%", xl: "45%" }}
           >
-            <Box
+            <Flex
               backgroundImage={orangeEllips}
               backgroundRepeat="no-repeat"
-              backgroundPosition="top right"
-              backgroundSize="40%"
+              backgroundPosition={{ m: "top right -150px", xl: "top right" }}
+              backgroundSize={{ s: "65%", xl: "40%" }}
+              py={{ s: "60px", xl: "0" }}
+              h="100vh"
+              justifyContent={{ s: "center", m: "start", xl: "space-between" }}
+              alignItems="center"
+              flexDirection={{ m: "column", xl: "row" }}
             >
-              <Flex
-                justifyContent={{ base: "center", m: "space-between" }}
-                alignItems="center"
-              >
-                {matches.m && (
-                  <Box h="100vh" w="100%">
-                    <SecondaryLogo />
-                  </Box>
-                )}
-                <Box
-                  background="rgba(255, 255, 255, 0.4)"
-                  backdropFilter="blur(25px)"
-                  pr="91px"
-                  pl="107px"
-                  h="100vh"
+              {matches.m && (
+                <Flex
+                  w="100%"
+                  flexDirection={{ base: "row", xl: "column" }}
+                  alignItems="center"
+                  justifyContent="center"
+                  mb={{ m: "50px", xl: "0" }}
                 >
-                  <LoginForm />
-                </Box>
+                  <Image
+                    src={loginPicture}
+                    alt="Login picture"
+                    maxWidth={{ m: "260px", xl: "435px" }}
+                    mb={{ base: "0", xl: "28px" }}
+                    mr={{ m: "40px" }}
+                  />
+                  <SecondaryLogo />
+                </Flex>
+              )}
+              <Flex
+                background={{ xl: "rgba(255, 255, 255, 0.4)" }}
+                backdropFilter={{ xl: "blur(25px)" }}
+                alignItems="center"
+                justifyContent="center"
+                px={{ m: "100px" }}
+                h={{ xl: "100vh" }}
+              >
+                <LoginForm />
               </Flex>
-            </Box>
+            </Flex>
           </Box>
         )}
       </Media>
