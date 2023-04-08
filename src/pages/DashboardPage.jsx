8bb257a18ce3d82849fa5f57a02f323f1ee3944b
nickgric <nick.gric@gmail.com>
2023-04-07 21:58:04 -0400

    logo

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import { Header } from "../components/Header";
 
-import { Currency } from "../components/Currency";
+import { Logo } from "../components/Logo";
 
 const DashboardPage = () => {
   return (
@@ -10,7 +10,6 @@ const DashboardPage = () => {
       <Header />
       <main>
         <Outlet />
-        <Currency />
       </main>
     </>
   );
