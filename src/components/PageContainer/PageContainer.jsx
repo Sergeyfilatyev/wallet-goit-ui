4190d354de940e4d758a86c4019bd84991f98a86
nickgric <nick.gric@gmail.com>
2023-04-12 19:19:23 -0400

    dashboard

--- src/components/PageContainer/PageContainer.jsx
+++ src/components/PageContainer/PageContainer.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 export const PageContainer = ({ children }) => {
   return (
-    <Box as="div" px={{ xs: "20px", m: "32px", l: "16px" }} width="100%">
+    <Box as="div" px={{ base: "20px", m: "32px", l: "16px" }} width="100%">
       {children}
     </Box>
   );
