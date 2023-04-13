780e7b4cb8bd27fa54b835fb425f5ac95a47106d
Valeria Likso <valerijasergievich@gmail.com>
2023-04-13 19:07:25 +0100

    add hocs

--- src/HOCs/PublicRoute.jsx
+++ src/HOCs/PublicRoute.jsx
@@ -1,3 +1,15 @@
-const PublicRoute = ({ children, restricted = false }) => {};
+import { useSelector, Outlet } from "react-redux";
+import { isUserLogin } from "../redux/auth/auth-selectors";
+import { Navigate } from "react-router-dom";
+
+const PublicRoute = () => {
+  const isAuth = useSelector(isUserLogin);
+
+  if (isAuth) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return <Outlet />;
+};
 
 export default PublicRoute;
