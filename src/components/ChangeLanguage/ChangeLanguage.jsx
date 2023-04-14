96d024f1adf2a6a5411f289093fe163c039f989a
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-04-14 16:36:52 +0300

    commit

--- src/components/ChangeLanguage/ChangeLanguage.jsx
+++ src/components/ChangeLanguage/ChangeLanguage.jsx
@@ -1,14 +1,70 @@
 import { useTranslation } from "react-i18next";
-import { Button } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Button,
+  Box,
+} from "@chakra-ui/react";
 
 export const ChangeLanguage = () => {
   const { i18n } = useTranslation();
 
   return (
-    <>
-      <Button onClick={() => i18n.changeLanguage("en")}>EN</Button>
-      <Button onClick={() => i18n.changeLanguage("ru")}>РУ</Button>
-      <Button onClick={() => i18n.changeLanguage("ua")}>УКР</Button>
-    </>
+    <Box
+      pos="absolute"
+      zIndex={"banner"}
+      top="4"
+      left="0"
+      right="0"
+      textAlign="center"
+      color={"#BDBDBD"}
+    >
+      <Menu>
+        {({ isOpen }) => (
+          <>
+            <MenuButton
+              as={Button}
+              aria-label="Language"
+              variant="ghost"
+              _hover={{ bg: "transparent" }}
+            >
+              {i18n.language === "en"
+                ? "EN"
+                : i18n.language === "ru"
+                ? "РУ"
+                : "УКР"}
+            </MenuButton>
+            <MenuList
+              opacity={isOpen ? "1" : "0"}
+              transition="opacity 300, transform 300"
+            >
+              <MenuItem
+                onClick={() => {
+                  i18n.changeLanguage("en");
+                }}
+              >
+                EN
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  i18n.changeLanguage("ru");
+                }}
+              >
+                РУ
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  i18n.changeLanguage("ua");
+                }}
+              >
+                УКР
+              </MenuItem>
+            </MenuList>
+          </>
+        )}
+      </Menu>
+    </Box>
   );
 };
