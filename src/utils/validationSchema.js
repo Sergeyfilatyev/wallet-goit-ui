9c29133dd6d129d687ae095cba4fddf339d6f8bd
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-04-15 17:11:51 +0300

    validate

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -10,14 +10,14 @@ export const validationSchemaRegister = Yup.object().shape({
   password: Yup.string()
     .min(8, "Password is too short")
     .matches(
-      "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?.,/-_&])[A-Za-zd@$!%*?.,/-_&]{8,}$",
+      "(?=.*[0-9])(?=.*[@$!%*?.,/-_&])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z@$!%*?.,/-_&]{8,}",
       "8+ characters: a-z, A-Z, 0-9, symbols(@$!%*?.,/-_&)"
     )
     .required("Password is required"),
   confirmPassword: Yup.string()
     .min(8, "Password is too short")
     .matches(
-      "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?.,/-_&])[A-Za-zd@$!%*?.,/-_&]{8,}$",
+      "(?=.*[0-9])(?=.*[@$!%*?.,/-_&])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z@$!%*?.,/-_&]{8,}",
       "8+ characters: a-z, A-Z, 0-9, symbols(@$!%*?.,/-_&)"
     )
     .required("Confirm password is required")
