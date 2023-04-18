d04dae19b9726c2cb3676dfc9b255c24a947f234
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-04-18 20:35:48 +0300

    modern-normalize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "modern-normalize/modern-normalize.css";
 import "./index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
