99b9a8459a81ed5951262c66e365a72b6a3bbcf9
Sergey Filatyev <filatevsergey2407@gmail.com>
2023-04-18 19:08:28 +0300

    routes

--- src/HOCs/PublicRoute.jsx
+++ src/HOCs/PublicRoute.jsx
@@ -1,10 +1,8 @@
 import { useSelector } from "react-redux";
 import { getAuth } from "../redux/auth/auth-selectors";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const PublicRoute = ({ children, restricted = false }) => {
-  const location = useLocation();
-  console.log(location);
   const isAuth = useSelector(getAuth);
   const shouldRedirect = isAuth && restricted;
   return shouldRedirect ? <Navigate to="/dashboard" /> : children;
